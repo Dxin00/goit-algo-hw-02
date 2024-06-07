d746dfa53d6b28303f8faf0ca6949e74eed32faf
Dxin00 <you@example.com>
2024-06-07 16:03:26 +0200

    commit 3

--- algo-hw-02-task-02.py
+++ algo-hw-02-task-02.py
@@ -1,3 +1,22 @@
-import queue
-import random
-import time
+from collections import deque
+
+def is_palindrome(input_string):
+    cleaned_string = ''.join(char.lower() for char in input_string if char.isalnum())
+    char_deque = deque(cleaned_string)
+    while len(char_deque) > 1:
+        if char_deque.popleft() != char_deque.pop():
+            return False
+    return True
+
+def main():
+    while True:
+        input_string = input("Введіть рядок для перевірки на паліндром (або 'q' для виходу): ").strip()
+        if input_string.lower() == 'q':
+            break
+        if is_palindrome(input_string):
+            print("Рядок є паліндромом!")
+        else:
+            print("Рядок не є паліндромом.")
+
+if __name__ == "__main__":
+    main()
